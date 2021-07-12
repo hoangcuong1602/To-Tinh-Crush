8884b8511b6375bfce860aba4dc9a174828c5758
hoangcuong1602 <87302476+hoangcuong1602@users.noreply.github.com>
2021-07-12 15:35:40 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Tên người yêu bạn à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    introTitle: 'Ngân à🥺',
+    introDesc: `là em đó, Nguyễn Huỳnh Thanh Ngân❤con tró rách của anh ^^`,
+    btnIntro: 'bấm vô đây tiếp nè bé',
+    title: 'cảm ơn em đã lựa chọn anh❤chúng mình cùng nhau đi thật lâu em nhé😙',
+    desc: 'em thử bấm vào không xem :), thách ớ ',
+    btnYes: 'oke ngy nhó❤❤ ',
+    btnNo: 'không nha liuliu',
+    question:'em có chắc với lựa chọn của mình hong công trúa <3 nếu k thì e có thể tắt web đi ạ =((',
+    btnReply: 'Gửi cho Cường ',
+    reply: 'Are you sure with your choice?',
+    mess: 'Anh biết em sẽ chắc mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'gặp lại a có quà cho bé nhó.',
+    btnAccept: 'Okie lun <3',
+    messLink: 'https://www.facebook.com/hoangcuong1602' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
